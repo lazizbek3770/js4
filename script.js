c93c95322fbf6dd8f6c854c0ee5613b408b5ad9d
lazizbek <lazizbekhasanov424@gmail.com>
2025-08-10 13:39:50 +0300

    js4

--- script.js
+++ script.js
@@ -9,21 +9,6 @@ function d(ism,a,b) {
 alert(d(ism,a,b))
 
 
-let miqdor = +prompt("Nechta misol ishlamoqchisiz?");
-let amallar = ["+", "-", "*", "/"];
 
-for (let i = 0; i < miqdor; i++) {
-  let a = Math.floor(Math.random() * 10) + 1;
-  let b = Math.floor(Math.random() * 10) + 1;
-  let amal = amallar[Math.floor(Math.random() * amallar.length)];
-  let javob = eval(`${a} ${amal} ${b}`);
-  
-  let age = prompt(`${a} ${amal} ${b} = ?`);
-  if (age == javob) {
-    console.log(`Sizning javob togri - ${javob}`);
-  } else {
-    console.log(`Sizning javob notogri - ${age} Togri javob  ${javob}`);
-  }
-}
 
 
