5fdee8954964ada4dc593d00347ec38d2301ccdc
lazizbek <lazizbekhasanov424@gmail.com>
2025-08-14 12:02:55 +0300

    js4

--- script.js
+++ script.js
@@ -10,5 +10,36 @@ alert(d(ism,a,b))
 
 
 
+let miqdor = +prompt("Nechta misol ishlamoqchisiz?");
+let amallar = ["+", "-", "*", "/"];
+
+for (let i = 0; i < miqdor; i++) {
+  let a = Math.floor(Math.random() * 10) + 1;
+  let b = Math.floor(Math.random() * 10) + 1;
+  let amal = amallar[Math.floor(Math.random() * amallar.length)];
+
+  if (amal === "/") {
+    a = a * b; 
+  }
+
+  let javob;
+  if (amal === "+") {
+    javob = a + b;
+  } else if (amal === "-") {
+    javob = a - b;
+  } else if (amal === "*") {
+    javob = a * b;
+  } else if (amal === "/") {
+    javob = a / b;
+  }
+
+  let foyJavob = +prompt(`${a} ${amal} ${b} = ?`);
+
+  if (foyJavob === javob) {
+    console.log(`Sizning javob togri - ${javob}`);
+  } else {
+    console.log(`Sizning javob notogri - ${foyJavob}. Togri javob: ${javob}`);
+  }
+}
 
 
